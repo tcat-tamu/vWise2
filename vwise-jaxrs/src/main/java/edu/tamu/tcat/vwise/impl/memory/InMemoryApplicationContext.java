95c50bdbdfa4db6f29ca1519c071a00912c21357
Neal Audenaert <neala@tamu.edu>
2017-03-03 20:31:44 -0600

    Add implementation stubs

---
+++ vwise-jaxrs/src/main/java/edu/tamu/tcat/vwise/impl/memory/InMemoryApplicationContext.java
@@ -0,0 +1,16 @@
+package edu.tamu.tcat.vwise.impl.memory;
+
+import edu.tamu.tcat.vwise.VwiseApplicationContext;
+import edu.tamu.tcat.vwise.WorkspaceRepository;
+
+public class InMemoryApplicationContext implements VwiseApplicationContext
+{
+
+   @Override
+   public WorkspaceRepository getRepository()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+}
---
+++ vwise-jaxrs/src/main/java/edu/tamu/tcat/vwise/impl/memory/WorkspaceRepoImpl.java
@@ -0,0 +1,61 @@
+package edu.tamu.tcat.vwise.impl.memory;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import edu.tamu.tcat.vwise.WorkspaceRepository;
+import edu.tamu.tcat.vwise.model.WorkspaceMeta;
+
+public class WorkspaceRepoImpl implements WorkspaceRepository
+{
+
+   @Override
+   public Collection<WorkspaceMeta> listAll()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public Optional<WorkspaceMeta> get(String id)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public Optional<WorkspaceMeta> get(String id, String version)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public WorkspaceMeta create(WorkspaceMeta data)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public WorkspaceMeta update(WorkspaceMeta data)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void remove(String id)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void purge(String id)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
